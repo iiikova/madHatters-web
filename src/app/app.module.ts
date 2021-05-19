ddd4d849b6b0d66b8ca0afc9334326d726b086d3
Ihor Koval <ikova.ua@gmail.com>
2021-05-19 14:52:17 +0300

    v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ListComponent } from './components/list/list.component';
+import { ImageTitleComponent } from './components/image-title/image-title.component';
+import { RangeComponent } from './components/range/range.component';
+import { QuoteComponent } from './components/quote/quote.component';
+import { NewsletterComponent } from './components/newsletter/newsletter.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ListComponent,
+    ImageTitleComponent,
+    RangeComponent,
+    QuoteComponent,
+    NewsletterComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/image-title/image-title.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-image-title',
+  templateUrl: './image-title.component.html',
+  styleUrls: ['./image-title.component.scss']
+})
+export class ImageTitleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
